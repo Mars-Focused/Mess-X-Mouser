9bc13738a0346bbf1fa5a4f840c6bba0c30dee24
jafe sellers <jafesellers@gmail.com>
2024-12-11 23:31:52 -0700

    add base interface and base level super classes

---
+++ Assets/Supers/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Supers
+{
+    abstract class Projectile
+    {
+        public Projectile()
+        {
+            //code to initialize a projectile. willprobaballyneed variables as inputs 
+        }
+    }
+}

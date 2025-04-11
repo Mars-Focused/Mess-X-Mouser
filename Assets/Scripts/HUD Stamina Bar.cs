eda1563186d89d5918ec93320e6e07edd2e4788a
MrBlueLynx <BlueLynx@live.com>
2025-04-10 18:22:06 -0600

    Created stamina Bar on the UI, still needs to be attached to stamina variable.

---
+++ Assets/Scripts/HUD Stamina Bar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDStaminaBar : MonoBehaviour
+{
+    private Image barImage;
+
+    private void Awake()
+    {
+        barImage = transform.Find("Bar").GetComponent<Image>();
+
+        barImage.fillAmount = 0.3f;
+    }
+}

c22acb61e70bd749b84bacc156d5ef250a40dcf6
Mars-Focused <marsthecoder@outlook.com>
2025-04-30 23:10:39 -0600

    Added MaxHealth and Alive bool to Enemy Super.

--- Assets/Supers/Enemy.cs
+++ Assets/Supers/Enemy.cs
@@ -15,15 +15,19 @@ namespace Assets.Supers
         {
             //code to init a base enemy
             this.health = 100;
+            this.maxHealth = 100;
             this.damage = 10;
             this.speed = 10;
+            this.alive = true;
         }
         public Enemy(float health, float damage, float speed)
         {
             //code to init an enemy with custom values
             this.health = health;
+            this.maxHealth = maxHealth;
             this.damage = damage;
             this.speed = speed;
+            this.alive = alive;
         }
 
         public void Damage(float ammount)

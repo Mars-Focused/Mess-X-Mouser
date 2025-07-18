3f85776a5b320382f68f1a3dd9558d021f9f49bd
MrBlueLynx <BlueLynx@live.com>
2025-07-18 15:27:24 -0600

    minor formatting.

--- Assets/Scripts/GameAssets.cs
+++ Assets/Scripts/GameAssets.cs
@@ -5,9 +5,15 @@ public class GameAssets : MonoBehaviour
 {
     private static GameAssets _i;
 
-    public static GameAssets i {
-        get {
-            if (_i == null) _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+    public static GameAssets i 
+    {
+        get 
+        {
+            if (_i == null)
+            {
+                _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+            }
+
             return _i;
         }
     }

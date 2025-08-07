7df6b5054f266b54927ae83417854776d7d28001
MrBlueLynx <BlueLynx@live.com>
2025-08-06 18:40:11 -0600

    Added a Simple (Terrible) Projectile Gun script

--- Assets/Scripts/Projectile Gun.cs
+++ Assets/Scripts/Projectile Gun.cs
@@ -1,39 +1,160 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+using UnityEngine;
+using TMPro;
+
+/// Thanks for downloading my projectile gun script! :D
+/// Feel free to use it in any project you like!
+/// 
+/// The code is fully commented but if you still have any questions
+/// don't hesitate to write a yt comment
+/// or use the #coding-problems channel of my discord server
+/// 
+/// Dave
 public class ProjectileGun : MonoBehaviour
 {
+    //bullet 
     public GameObject bullet;
 
+    //bullet force
     public float shootForce, upwardForce;
-    public int magazineSize, bulletsPerTap;
+
+    //Gun stats
+    public float timeBetweenShooting;
+    public float spread;
+    public float reloadTime;
+    public float timeBetweenShots;
+    public int magazineSize;
+    public int bulletsPerTap;
     public bool allowButtonHold;
 
-    int bulletsLeft, bulletsShot;
+    int bulletsLeft;
+    int bulletsShot;
 
+    //Recoil
+    public Rigidbody playerRb;
+    public float recoilForce;
+
+    //bools
     bool shooting, readyToShoot, reloading;
 
+    //Reference
     public Camera fpsCam;
     public Transform attackPoint;
 
+    //Graphics
+    public GameObject muzzleFlash; //TODO: Add Muzzle Flash Maybe
+    public TextMeshProUGUI ammunitionDisplay; //TODO: Add Amunition Display
+
+    //bug fixing :D
     public bool allowInvoke = true;
 
     private void Awake()
     {
+        //make sure magazine is full
         bulletsLeft = magazineSize;
         readyToShoot = true;
     }
 
-    private void oldInputMethod() //Change with New input system
+    private void Update()
     {
-        if (allowButtonHold)
+        OldInputMethod();
+
+        //Set ammo display, if it exists :D
+        if (ammunitionDisplay != null)
+            ammunitionDisplay.SetText(bulletsLeft / bulletsPerTap + " / " + magazineSize / bulletsPerTap);
+    }
+    private void OldInputMethod() //TODO: Change to the New Input System
+    {
+        //Check if allowed to hold down button and take corresponding input
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        //Reloading 
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+        //Reload automatically when trying to shoot without ammo
+        if (readyToShoot && shooting && !reloading && bulletsLeft <= 0) Reload();
+
+        //Shooting
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
         {
-            shooting = Input.GetKey(KeyCode.Mouse0);
+            //Set bullets shot to 0
+            bulletsShot = 0;
+
+            Shoot();
         }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //Find the exact hit position using a raycast
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Just a ray through the middle of your current view
+        RaycastHit hit;
+
+        //check if ray hits something
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
         else
+            targetPoint = ray.GetPoint(75); //Just a point far away from the player
+
+        //Calculate direction from attackPoint to targetPoint
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        //Calculate spread TODO: THIS IS JUST FUCKING STUPID AND HE WOULD HAVE REALIZED IT IF HE HAD DONE ANY CLOSE-RANGE TESTING
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //Calculate new direction with spread
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0); //Just add spread to last direction
+
+        //Instantiate bullet/projectile
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); //store instantiated bullet in currentBullet
+        //Rotate bullet to shoot direction
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        //Add forces to bullet
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        //Instantiate muzzle flash, if you have one
+        if (muzzleFlash != null)
+            Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        //Invoke resetShot function (if not already invoked), with your timeBetweenShooting
+        if (allowInvoke)
         {
-            shooting = Input.GetKeyDown(KeyCode.Mouse0);
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+
+            //Add recoil to player (should only be called once)
+            playerRb.AddForce(-directionWithSpread.normalized * recoilForce, ForceMode.Impulse);
         }
+
+        //if more than one bulletsPerTap make sure to repeat shoot function
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);
+    }
+    private void ResetShot()
+    {
+        //Allow shooting and invoking again
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime); //Invoke ReloadFinished function with your reloadTime as delay
+    }
+    private void ReloadFinished()
+    {
+        //Fill magazine
+        bulletsLeft = magazineSize;
+        reloading = false;
     }
 }

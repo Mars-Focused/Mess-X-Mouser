0e49775c944db7283f16285b0c6111331ed2486f
marsthecoder@outlook.com <marsthecoder@outlook.com>
2025-12-27 00:09:31 -0700

    Attempting to Deal damage, Unsucsessful.

--- Assets/BaseProjectileScript.cs
+++ Assets/BaseProjectileScript.cs
@@ -7,6 +7,7 @@ public class BaseProjectileScript : MonoBehaviour
 {
     private Rigidbody rb;
     private float Lifetime = 2.5f;
+    public int damage;
 
     private void Start()
     {
@@ -16,13 +17,28 @@ public class BaseProjectileScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Ran into Object");
+        if (collision.gameObject.name != this.name)
+        {
+            DestroyGameObject();
+            Debug.Log("ran Into a " + collision.gameObject.name);
+        }
+
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            var targetScript = collision.gameObject.GetComponent<Enemy_AI2>(); // Change to a seperate Health component.
+            if (targetScript != null)
+            {
+                Debug.Log("Dealt Damage");
+                targetScript.Damage(damage);
+            }
+        }
         DestroyGameObject();
     }
 
+
     private void DestroyGameObject()
     {
-        Debug.Log("Destroying Projectile");
+        //Debug.Log("Destroying Projectile");
         Destroy(gameObject);
     }
 }
